fa50c66506d86885532925a72ee2425e996b0e95
alexvidenov <a.videnov8a@gmail.com>
2020-06-03 10:20:01 +0300

    [UPDATE] Set up the viewpager with the bottom navigation view and added fragment xml boilerplate.

--- Todoapp/app/src/main/java/com/example/todoapp/activities/Activity.java
+++ Todoapp/app/src/main/java/com/example/todoapp/activities/Activity.java
@@ -1,17 +1,33 @@
 package com.example.todoapp.activities;
 
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 
+import com.example.todoapp.R;
 import com.example.todoapp.ui.adapter.FragmentPageAdapter;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public abstract class Activity extends AppCompatActivity {
+    protected BottomNavigationView bottomNavigationView;
     protected FragmentPageAdapter fragmentPageAdapter; // init in child classes...
     protected ViewPager viewPager;
 
-    public Activity(FragmentPageAdapter fragmentPageAdapter, ViewPager viewPager) {
-        this.fragmentPageAdapter = fragmentPageAdapter;
-        this.viewPager = viewPager;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        viewPager = findViewById(R.id.view_pager);
+    }
+
+    public BottomNavigationView getBottomNavigationView() {
+        return bottomNavigationView;
+    }
+
+    public void setBottomNavigationView(BottomNavigationView bottomNavigationView) {
+        this.bottomNavigationView = bottomNavigationView;
     }
 
     public FragmentPageAdapter getFragmentPageAdapter() {
--- Todoapp/app/src/main/java/com/example/todoapp/activities/MainActivity.java
+++ Todoapp/app/src/main/java/com/example/todoapp/activities/MainActivity.java
@@ -1,17 +1,84 @@
 package com.example.todoapp.activities;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import com.example.todoapp.R;
+import com.example.todoapp.ui.adapter.FragmentPageAdapter;
+import com.example.todoapp.ui.adapter.SetUpActivity;
+import com.example.todoapp.ui.ui_core.CreateToDoFragment;
+import com.example.todoapp.ui.ui_core.EditToDoFragment;
+import com.example.todoapp.ui.ui_core.ViewToDoFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements BottomNavigationView.OnNavigationItemSelectedListener,
+                                                      ViewPager.OnPageChangeListener, SetUpActivity {
+    private MenuItem menuItem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        super.onCreate(savedInstanceState); // calls Activity.onCreate, initializing the viewpager and navigation
         setContentView(R.layout.activity_main);
 
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+
+        initFragmentPageAdapter();
+        setUpViewPager(viewPager);
     }
 
+    // bottom_navigation_listener
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()){
+            case R.id.action_create_todo:
+                viewPager.setCurrentItem(0);
+                break;
+            case R.id.action_edit_todo:
+                viewPager.setCurrentItem(1);
+                break;
+            case R.id.action_view_todo:
+                viewPager.setCurrentItem(2);
+                break;
+        }
+        return false;
+    }
+
+    // viewPager_change_listener
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) { }
+
+    @Override
+    public void onPageSelected(int position) {
+        if(menuItem != null) {
+            menuItem.setChecked(false);
+        } else {
+            bottomNavigationView.getMenu().getItem(0).setChecked(false);
+        }
+
+        MenuItem currentItem = bottomNavigationView.getMenu().getItem(position);
+        currentItem.setChecked(true);
+        menuItem = currentItem;
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) { }
+
+    @Override
+    public void setUpViewPager(ViewPager viewPager) {
+        viewPager.addOnPageChangeListener(this);
+        viewPager.setAdapter(fragmentPageAdapter);
+    }
+
+    @Override
+    public void initFragmentPageAdapter() {
+        fragmentPageAdapter = new FragmentPageAdapter(getSupportFragmentManager(), this);
+
+        fragmentPageAdapter.addFragments(new CreateToDoFragment(), "createToDoFragment");
+        fragmentPageAdapter.addFragments(new EditToDoFragment(), "editToDoFragment");
+        fragmentPageAdapter.addFragments(new ViewToDoFragment(), "viewToDoFragment");
+    }
 }
